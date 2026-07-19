048781eb60f175191e3cba103d9d953dcb808838
israel2002551 <134238479+israel2002551@users.noreply.github.com>
2026-07-19 17:34:29 +0100

    Update Supabase configuration with new credentials

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.MKV_SUPABASE_CONFIG = {
-  SUPABASE_URL: "",
-  SUPABASE_ANON_KEY: "",
+  SUPABASE_URL: "https://knujbawjuykwftawkcte.supabase.co",
+  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtudWpiYXdqdXlrd2Z0YXdrY3RlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQ0NzIyMTYsImV4cCI6MjEwMDA0ODIxNn0.HKGeufrdrahwz3woWuylz-Z1vxmwL9QzMjy0zTdfl4Y",
 };
